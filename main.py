718d8d53eece226a90bf5816690f219e565f33e8
github_daraujop <daraujop77@gmail.com>
2025-08-19 23:54:58 -0500

    Printing Report

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from stats import count_words
+from stats import char_rep
+from stats import sort_dict
 
 def get_book_text(filepath):
 	with open(filepath) as f:
@@ -7,9 +9,16 @@ def get_book_text(filepath):
 
 
 def main():
+	import pprint
 	path = "/home/daraujopaez/github/bookbot/books/frankenstein.txt"
 	book_text =  get_book_text(path)
-	print(f"{count_words(book_text)} words found in the document"  )
-
+	print("============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...\n----------- Word Count ----------")
+	print(f"Found {count_words(book_text)} total words")
+	print("--------- Character Count -------")
+	chars = char_rep(book_text)
+	sorted_chars = sort_dict(chars)
+	for char in sorted_chars:
+		print(f"{char['char']}: {char['num']}")		
+	print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,5 +1,31 @@
 
+def sort_key(e):
+	return e['num']
+
+def sort_dict(dict_new):
+	new_list = []
+	sorted_dict = {}
+	for char, num in dict_new.items():
+		if char.isalpha():
+			new_list.append({"char" : char, "num": num})
+	
+	new_list.sort(key=sort_key, reverse=True)
+#	sorted_dict = dict(new_list)
+#	print(type(sorted_dict))
+	return new_list		
+	
 def count_words(books_content):
 	word_list = []
 	word_list = books_content.split()
 	return len(word_list)
+
+def char_rep(book_content):
+	dict = {}
+	book_content = book_content.lower()
+	for char in book_content:
+		if char in dict:
+			dict[char] += 1
+		else:
+			dict[char] = 1
+	
+	return dict
