a915fde79b644ca3e2bf3b8eeae1d7cc2729b80e
github_daraujop <daraujop77@gmail.com>
2025-08-19 22:31:06 -0500

    Added functions to count words in the book

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("greetings boots")
+from stats import count_words
+
+def get_book_text(filepath):
+	with open(filepath) as f:
+		file_content = f.read()
+	return file_content
+
+
+def main():
+	path = "/home/daraujopaez/github/bookbot/books/frankenstein.txt"
+	book_text =  get_book_text(path)
+	print(f"{count_words(book_text)} words found in the document"  )
+
+
+main()
---
+++ stats.py
@@ -0,0 +1,5 @@
+
+def count_words(books_content):
+	word_list = []
+	word_list = books_content.split()
+	return len(word_list)
